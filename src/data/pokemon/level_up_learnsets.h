751b6178d827c40dda3a09175d6e677f89bc3674
Thea Fortin <thea@kali>
2024-08-03 13:56:43 -0400

    Fixed substitute (replaced doll with blahaj)

--- src/data/pokemon/level_up_learnsets.h
+++ src/data/pokemon/level_up_learnsets.h
@@ -3677,6 +3677,7 @@ static const u16 sBlazikenLevelUpLearnset[] = {
 static const u16 sMudkipLevelUpLearnset[] = {
     LEVEL_UP_MOVE( 1, MOVE_TACKLE),
     LEVEL_UP_MOVE( 1, MOVE_GROWL),
+    LEVEL_UP_MOVE( 1, MOVE_SUBSTITUTE),
     LEVEL_UP_MOVE( 6, MOVE_MUD_SLAP),
     LEVEL_UP_MOVE(10, MOVE_WATER_GUN),
     LEVEL_UP_MOVE(15, MOVE_BIDE),
